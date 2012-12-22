f824e284f2ad245e76d4ef02d8c871c1e405d193
Adriel <hadriel84@hotmail.com>
2012-12-22 13:00:21 -0600

    initialize rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GitTest::Application.config.secret_token = '0813f5c04b089dde614ea3c2625563b879ebe55f4da3403797a38810791a9377262a9217f72e94fe1c09bd2aac5b997079d84365495cbc1e38538402c187ee3f'
